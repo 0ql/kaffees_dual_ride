1b1c292c3b26ab327aadd9bf56671c1c6600fe92
Sqaaakoi <sqaaakoi@sqaaakoi.xyz>
2022-11-23 02:54:12 +1300

    Switch to mixin for mounting the horse
    This does 2 things;
    - it adds the hand swinging animation
    - removes Fabric API dependency

--- src/main/java/de/tobiasvonmassow/kaffees_dual_ride/Kaffees_Dual_Ride.java
+++ src/main/java/de/tobiasvonmassow/kaffees_dual_ride/Kaffees_Dual_Ride.java
@@ -1,23 +1,9 @@
 package de.tobiasvonmassow.kaffees_dual_ride;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-
-import net.minecraft.util.ActionResult;
 
 public class Kaffees_Dual_Ride implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		UseEntityCallback.EVENT.register((player, world, hand, pos, hitResult) -> {
-			if (!player.isSpectator() && !player.isSneaking()) {
-				final Entity target = hitResult.getEntity();
-				if (target instanceof AbstractHorseEntity && target.getPassengerList().size() < 2) {
-					player.startRiding(target, true);
-				}
-			}
-			return ActionResult.PASS;
-		});
 	}
 }
--- src/main/java/de/tobiasvonmassow/kaffees_dual_ride/mixin/Kaffees_Dual_Ride_Mixin.java
+++ src/main/java/de/tobiasvonmassow/kaffees_dual_ride/mixin/Kaffees_Dual_Ride_Mixin.java
@@ -1,16 +1,31 @@
 package de.tobiasvonmassow.kaffees_dual_ride.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AbstractHorseEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import com.google.common.collect.ImmutableList;
+
 @Mixin(AbstractHorseEntity.class)
-public class Kaffees_Dual_Ride_Mixin {
+public abstract class Kaffees_Dual_Ride_Mixin extends AnimalEntity {
+
+	protected Kaffees_Dual_Ride_Mixin(EntityType<? extends AbstractHorseEntity> arg, World arg2) {
+		super((EntityType<? extends AnimalEntity>)arg, arg2);
+    }
+
 	@Inject(at = @At(value = "TAIL"), method = "updatePassengerPosition(Lnet/minecraft/entity/Entity;)V", locals = LocalCapture.CAPTURE_FAILHARD)
 	private void injected(Entity passenger, CallbackInfo ci) {
 		// don't reposition riders if there is only one rider
@@ -25,4 +40,25 @@ public class Kaffees_Dual_Ride_Mixin {
 		Vec3d vec3d = new Vec3d(x_offset, 0.0, 0.0).rotateY(-((AbstractHorseEntity)(Object)this).getYaw() * ((float)Math.PI / 180) - 1.5707964f);
 		passenger.setPosition(((AbstractHorseEntity)(Object)this).getX() + vec3d.x,((AbstractHorseEntity)(Object)this).getY() + (double)horizontal_offset,((AbstractHorseEntity)(Object)this).getZ() + vec3d.z);
 	}
+	
+	// abstract class + constructor required so I can extend AnimalEntity to perform this explicit override 
+	@Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        if (this.getPassengerList().size() > 0 && this.canAddPassenger(player)) {
+			player.startRiding(this);
+			return ActionResult.success(this.world.isClient);
+        }
+		return super.interactMob(player, hand);
+    }
+
+    @Override
+    protected boolean canAddPassenger(Entity passenger) {
+        return this.getPassengerList().size() < this.getMaxPassengers();
+    }
+
+    protected int getMaxPassengers() {
+        return 2;
+    }
+
+	
 }
