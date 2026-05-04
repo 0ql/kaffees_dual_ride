7b4c7c7c7b60531c844bef0be1e5afe287a0cfb1
Kaffee <tobi.von.massow@gmail.com>
2026-05-04 22:55:32 +0200

    fix taming and bugs related to haspassengers

--- src/main/java/de/tobiasvonmassow/kaffees_dual_ride/mixin/Kaffees_Dual_Ride_Mixin.java
+++ src/main/java/de/tobiasvonmassow/kaffees_dual_ride/mixin/Kaffees_Dual_Ride_Mixin.java
@@ -4,6 +4,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -15,8 +19,22 @@ import org.spongepowered.asm.mixin.Shadow;
 public abstract class Kaffees_Dual_Ride_Mixin extends AnimalEntity {
 
 	protected Kaffees_Dual_Ride_Mixin(EntityType<? extends AbstractHorseEntity> arg, World arg2) {
-		super((EntityType<? extends AnimalEntity>) arg, arg2);
+		super(arg, arg2);
 	}
+
+	@Shadow public abstract boolean isTame();
+
+	@Shadow public abstract void openInventory(PlayerEntity player);
+
+	@Shadow public abstract boolean hasArmorSlot();
+
+	@Shadow protected abstract void putPlayerOnBack(PlayerEntity player);
+
+	@Shadow public abstract void equipHorseArmor(PlayerEntity player, ItemStack itemStack);
+
+	@Shadow public abstract boolean hasArmorInSlot();
+
+	@Shadow public abstract boolean isHorseArmor(ItemStack itemStack);
 
 	@Shadow
 	private float lastAngryAnimationProgress;
@@ -37,8 +55,29 @@ public abstract class Kaffees_Dual_Ride_Mixin extends AnimalEntity {
 	}
 
 	@Override
-	public boolean hasPassengers() {
-		return false;
+	public ActionResult interactMob(PlayerEntity player, Hand hand) {
+		if (this.isBaby()) {
+			return super.interactMob(player, hand);
+		} else if (this.isTame() && player.shouldCancelInteraction()) {
+			this.openInventory(player);
+			return ActionResult.success(this.getWorld().isClient);
+		} else {
+			ItemStack itemStack = player.getStackInHand(hand);
+			if (!itemStack.isEmpty()) {
+				ActionResult actionResult = itemStack.useOnEntity(player, this, hand);
+				if (actionResult.isAccepted()) {
+					return actionResult;
+				}
+
+				if (this.hasArmorSlot() && this.isHorseArmor(itemStack) && !this.hasArmorInSlot()) {
+					this.equipHorseArmor(player, itemStack);
+					return ActionResult.success(this.getWorld().isClient);
+				}
+			}
+
+			this.putPlayerOnBack(player);
+			return ActionResult.success(this.getWorld().isClient);
+		}
 	}
 
 	@Override
