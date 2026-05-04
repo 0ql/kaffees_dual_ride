e2cb7355bb9b49180ec475554c7b8a4cadd9da6b
Kaffee <tobi.von.massow@gmail.com>
2026-05-04 15:48:11 +0200

    update gradle, loom and fix like 20bugs related to overriding the entire interactmob method

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 
 This is a tiny mod that allows two players to sit on a horse. It is required on Client and Server. It is licensed under GPLv3. Sourcecode available on [Github](https://github.com/0ql/kaffees_dual_ride).
 
+## Dev
+
+To start two minecraft clients at once:
+```bash
+./gradlew launch2clients
+```
+
 [Curseforge](https://www.curseforge.com/minecraft/mc-mods/kaffees-dual-ride)
 [Modrinth](https://modrinth.com/mod/kaffees_dual_ride)
--- src/main/java/de/tobiasvonmassow/kaffees_dual_ride/mixin/Kaffees_Dual_Ride_Mixin.java
+++ src/main/java/de/tobiasvonmassow/kaffees_dual_ride/mixin/Kaffees_Dual_Ride_Mixin.java
@@ -2,21 +2,15 @@ package de.tobiasvonmassow.kaffees_dual_ride.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.HorseArmorItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.SaddleItem;
-import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -24,21 +18,25 @@ import java.util.UUID;
 
 @Mixin(AbstractHorseEntity.class)
 public abstract class Kaffees_Dual_Ride_Mixin extends AnimalEntity {
-	private static final UUID HORSE_ARMOR_BONUS_ID = UUID.fromString("556E1665-8B10-40C8-8F9D-CF9B1667F295"); //Used for attribute modifier when equipping armor
 	protected Kaffees_Dual_Ride_Mixin(EntityType<? extends AbstractHorseEntity> arg, World arg2) {
 		super(arg, arg2);
 	}
 
-	@Shadow
-	private float lastAngryAnimationProgress;
+	@Shadow private float lastAngryAnimationProgress;
 
 	@Shadow public abstract void equipHorseArmor(PlayerEntity player, ItemStack stack);
 
 	@Shadow public abstract boolean hasArmorInSlot();
 
-	@Shadow public abstract void saddle(@Nullable SoundCategory sound);
+	@Shadow public abstract boolean isTame();
 
-	@Shadow public abstract boolean isSaddled();
+	@Shadow public abstract void openInventory(PlayerEntity player);
+
+	@Shadow public abstract boolean hasArmorSlot();
+
+	@Shadow public abstract boolean isHorseArmor(ItemStack item);
+
+	@Shadow protected abstract void putPlayerOnBack(PlayerEntity player);
 
 	@Override
 	public void updatePassengerPosition(Entity passenger, Entity.PositionUpdater positionUpdater) {
@@ -55,40 +53,29 @@ public abstract class Kaffees_Dual_Ride_Mixin extends AnimalEntity {
 		positionUpdater.accept(passenger, this.getX() + vec3d.x + (double) (h * f), this.getY() + this.getMountedHeightOffset() + passenger.getHeightOffset() + (double) i, this.getZ() + vec3d.z - (double) (h * g));
 	}
 
-	// abstract class + constructor required, so I can extend AnimalEntity to
-	// perform this explicit override
 	@Override
 	public ActionResult interactMob(PlayerEntity player, Hand hand) {
-		if(!player.getStackInHand(hand).isEmpty()) {
-			if (this.getFirstPassenger() == null) {
-				if (player.getStackInHand(hand).getItem() instanceof HorseArmorItem && !this.hasArmorInSlot()) {
-					customEquipHorseArmor(player.getStackInHand(hand), player);
-				} else if (player.getStackInHand(hand).getItem() instanceof SaddleItem && !this.isSaddled()) {
-					if(!player.getAbilities().creativeMode) {
-						player.getStackInHand(hand).decrement(1);
-					}
-					this.saddle(SoundCategory.NEUTRAL);
-				}
-			}
-			return ActionResult.SUCCESS;
-		}
-		if (this.canAddPassenger(player)) {
-			player.startRiding(this);
+		if (this.isBaby()) {
+			return super.interactMob(player, hand);
+		} else if (this.isTame() && player.shouldCancelInteraction()) {
+			this.openInventory(player);
 			return ActionResult.success(this.getWorld().isClient);
-		}
-		return super.interactMob(player, hand);
-	}
+		} else {
+			ItemStack itemStack = player.getStackInHand(hand);
+			if (!itemStack.isEmpty()) {
+				ActionResult actionResult = itemStack.useOnEntity(player, this, hand);
+				if (actionResult.isAccepted()) {
+					return actionResult;
+				}
 
-	private void customEquipHorseArmor(ItemStack stack, PlayerEntity player) {
-		if(stack.getItem() instanceof HorseArmorItem) {
-			this.equipHorseArmor(player, stack);
-			if (!this.getWorld().isClient) {
-				this.getAttributeInstance(EntityAttributes.GENERIC_ARMOR).removeModifier(HORSE_ARMOR_BONUS_ID);
-				int i = ((HorseArmorItem) stack.getItem()).getBonus();
-				if (i != 0) {
-					this.getAttributeInstance(EntityAttributes.GENERIC_ARMOR).addTemporaryModifier(new EntityAttributeModifier(HORSE_ARMOR_BONUS_ID, "Horse armor bonus", (double) i, EntityAttributeModifier.Operation.ADDITION));
+				if (this.hasArmorSlot() && this.isHorseArmor(itemStack) && !this.hasArmorInSlot()) {
+					this.equipHorseArmor(player, itemStack);
+					return ActionResult.success(this.getWorld().isClient);
 				}
 			}
+
+			this.putPlayerOnBack(player);
+			return ActionResult.success(this.getWorld().isClient);
 		}
 	}
 
